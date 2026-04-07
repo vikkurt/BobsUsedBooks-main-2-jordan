5398d7d8f863ca3ef679b6db09f1c67c3d8d14dc
NoReply Amazon <no-reply@amazon.com>
2026-04-07 00:28:53 +0000

    DB transformation: Upload transformed code

--- app/Bookstore.Domain/Entity.cs
+++ app/Bookstore.Domain/Entity.cs
@@ -1,18 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bookstore.Domain
 {
     public abstract class Entity
     {
+        [Key]
+        [Column("id")]
         public int Id { get; set; }
 
+        [Column("created_by")]
         public string CreatedBy { get; set; } = "System";
 
+        [Column("created_on")]
         public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
 
+        [Column("updated_on")]
         public DateTime UpdatedOn { get; set; } = DateTime.UtcNow;
 
         [Timestamp]
+        [Column("row_version")]
         public byte[] RowVersion { get; set; }
 
         public bool IsNewEntity()
--- app/Bookstore.Web/Startup/ServicesSetup.cs
+++ app/Bookstore.Web/Startup/ServicesSetup.cs
@@ -5,7 +5,6 @@ using Amazon.SecretsManager;
 using Bookstore.Data;
 using Bookstore.Domain.AdminUser;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +12,9 @@ using System.Text.Json;
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Npgsql;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 
 namespace Bookstore.Web.Startup
 {
@@ -31,7 +33,7 @@ namespace Bookstore.Web.Startup
             builder.Services.AddAWSService<IAmazonRekognition>();
 
             var connString = GetDatabaseConnectionString(builder.Configuration);
-            builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(connString));
+            builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseNpgsql(connString));
             builder.Services.AddSession();
 
             return builder;
